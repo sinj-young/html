ed7d19a8eeb016b48032d16119496d2f36290810
sinj-young <hanj411@gmail.com>
2022-04-23 18:36:29 +0900

    feat: index files update

---
+++ index.js
@@ -0,0 +1,50 @@
+console.log("im working.")
+//varible
+const c = 5
+let a=221
+let b= a-5
+
+console.log(b)
+
+/*
+todo list
+todo list
+todo list
+*/
+
+//String
+const what = "text"
+//const what = text (x)
+//you can constant something to emoji
+//console.log(what)
+
+//Boolean "true-1 false-0"
+const wat = true;
+
+//Number
+const wa = 5;
+
+//FLoat
+const ww = 55.234;
+
+
+
+//Array
+/*
+const monday = "Mon";
+const tuesday = "Mon";
+const wedsenday = "Mon";
+const thursday = "s";
+const friday = "Fri";
+const saturday = "Sat";
+*/
+
+/*
+const daysOfWeek = ["MON","TUE","WED","THU","FRI","SAT","SUN",true];
+console.log(daysOfWeek[2]);
+*/
+
+
+//Object
+
+
